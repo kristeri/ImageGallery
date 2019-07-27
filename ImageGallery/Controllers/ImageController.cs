fb6a80a78ee1e37b2ee9108af4b66219c6176749
Krister Torikka <krister.torikka@gmail.com>
2019-07-28 00:11:36 +0300

    Add form for uploading images

---
+++ ImageGallery/Controllers/ImageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageGallery.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Upload()
+        {
+            var model = new UploadImageModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult UploadNewImage()
+        {
+            return Ok();
+        }
+    }
+}
