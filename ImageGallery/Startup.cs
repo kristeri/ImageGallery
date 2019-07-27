81ceb9bb2b86cbd476bb284e40597ab397dc7300
Krister Torikka <krister.torikka@gmail.com>
2019-07-27 23:13:01 +0300

    Edit navbar buttons, make images clickable

--- ImageGallery/Startup.cs
+++ ImageGallery/Startup.cs
@@ -62,7 +62,7 @@ namespace ImageGallery
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Gallery}/{action=Index}/{id?}");
             });
         }
     }
