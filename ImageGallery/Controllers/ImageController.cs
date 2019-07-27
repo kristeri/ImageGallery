90732a842b06f37777815f780aa371e3690eb059
Krister Torikka <krister.torikka@gmail.com>
2019-07-28 02:08:22 +0300

    Add function for uploading file

--- ImageGallery/Controllers/ImageController.cs
+++ ImageGallery/Controllers/ImageController.cs
@@ -1,14 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ImageGallery.Data;
 using ImageGallery.Models;
+using ImageGallery.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ImageGallery.Controllers
 {
     public class ImageController : Controller
     {
+        private IConfiguration _config;
+        private IImage _imageservice;
+        private string AzureConnectionString { get; set; }
+
+        public ImageController(IConfiguration config, IImage imageService)
+        {
+            _config = config;
+            _imageservice = imageService;
+            AzureConnectionString = _config["AzureStorageConnectionString"];
+        }
+
         public IActionResult Upload()
         {
             var model = new UploadImageModel();
@@ -16,9 +29,17 @@ namespace ImageGallery.Controllers
         }
 
         [HttpPost]
-        public IActionResult UploadNewImage()
+        public async Task<IActionResult> UploadNewImage(IFormFile file, string tags, string title)
         {
-            return Ok();
+            var container = _imageservice.GetBlobContainer(AzureConnectionString, "images");
+            var content = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+            var fileName = content.FileName.Trim('"');
+
+            var blockBlob = container.GetBlockBlobReference(fileName);
+            await blockBlob.UploadFromStreamAsync(file.OpenReadStream());
+            await _imageservice.SetImage(title, tags, blockBlob.Uri);
+
+            return RedirectToAction("Index", "Gallery");
         }
     }
 }
